eecffd225033a23adcd0db79d07a6d8a7f5d4b61
Ege <egealper54@gmail.com>
2025-07-13 15:06:28 +0300

    Update party colors: AP=#750a6f, DP=#4a4d04, CGP=#5a5f63

--- out/html/d3-linegraph.js
+++ out/html/d3-linegraph.js
@@ -19,7 +19,7 @@ d3.linegraph = function(noTicks, noDots, parties, partyColors, partyNames, dataM
         parties = ['chp', 'TIP', 'CGP', 'z', 'DP', 'MSP', 'MHP', 'other'];
     }
     if (!partyColors) {
-        partyColors = {'chp': '#E3000F', 'TIP': '#8B0000', 'CGP': '#5a5f63', 'z': '#000', 'DP': '#750a6f', 'MSP': '#054521', 'MHP': '#954B00', 'other': '#a0a0a0'};
+        partyColors = {'chp': '#E3000F', 'TIP': '#8B0000', 'CGP': '#5a5f63', 'z': '#750a6f', 'DP': '#4a4d04', 'MSP': '#054521', 'MHP': '#954B00', 'other': '#a0a0a0'};
     }
     if (!partyNames) {
         partyNames = {'chp': 'CHP', 'TIP': 'TIP', 'CGP': 'CGP', 'z': 'AP', 'DP': 'DP', 'MSP': 'MSP', 'MHP': 'MHP', 'other': 'Others'};
