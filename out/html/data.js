eb2cba50f5eac3bc4f469c481b4635aea394abfc
alllllllllllllllllllllllllllllllllll <111067206+alllllllllllllllllllllllllllllllllll@users.noreply.github.com>
2025-11-25 19:23:14 +0300

    Create data.js

---
+++ out/html/data.js
@@ -0,0 +1,12 @@
+const tooltipList = [{
+    searchString: "placeholder",
+    explanationText: "<img src=img/___.png> *placeholder*"
+}];
+
+
+const colourList = [{
+        word: "PS",
+        style: "color: #BA0000; font-weight: bold;",
+        img: "img/ps.png"
+    }
+];
