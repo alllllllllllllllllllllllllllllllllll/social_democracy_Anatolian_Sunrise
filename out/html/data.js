3dfb3a7a06c447779453a2b33f0cc0f0faafc095
alllllllllllllllllllllllllllllllllll <111067206+alllllllllllllllllllllllllllllllllll@users.noreply.github.com>
2025-11-25 20:13:18 +0300

    Update data.js

--- out/html/data.js
+++ out/html/data.js
@@ -1,12 +1,12 @@
 const tooltipList = [{
-    searchString: "placeholder",
-    explanationText: "<img src=img/___.png> *placeholder*"
+    searchString: "CHP",
+    explanationText: "<img src=img/chp5_20251125_200815_0000.svg> *Republican People's Party, aka CHP. The party of Kemal Atatürk and the representative of the enlightenment values in Turkey.*"
 }];
 
 
 const colourList = [{
         word: "CHP",
         style: "color: #E3000F; font-weight: bold;",
-        img: "img/CHP_six_arrows.svg.png"
+        img: "x.png"
     }
 ];
