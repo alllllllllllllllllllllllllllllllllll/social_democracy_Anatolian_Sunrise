b366b820318c0ceffd689abb5bddbb41db75d598
alllllllllllllllllllllllllllllllllll <111067206+alllllllllllllllllllllllllllllllllll@users.noreply.github.com>
2025-11-27 15:22:12 +0300

    Revise tooltipList with new image and explanation
    Updated tooltipList to include image source and text.

--- out/html/data.js
+++ out/html/data.js
@@ -1,11 +1,11 @@
-const tooltipList = [{
-    searchString: "CHP",
-    explanationText: "<img/chp5_20251125_200815_0000.svg>"
-}];
-
-
 const colourList = [{
         word: "CHP",
         style: "color: #E3000F; font-weight: bold;"
     }
 ];
+
+
+const tooltipList = [{
+    searchString: "CHP",
+    explanationText: "<img src=img/chp5_20251125_200815_0000.svg> Republican People's Party"
+}];
