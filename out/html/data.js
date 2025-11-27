952935aafea90a5aca2c3678421cf0742314ef2e
alllllllllllllllllllllllllllllllllll <111067206+alllllllllllllllllllllllllllllllllll@users.noreply.github.com>
2025-11-27 15:05:30 +0300

    Update data.js

--- out/html/data.js
+++ out/html/data.js
@@ -1,6 +1,6 @@
 const tooltipList = [{
     searchString: "CHP",
-    explanationText: "<img/chp5_20251125_200815_0000.svg> *Republican People's Party, aka CHP. The party of Kemal Atatürk and the representative of the enlightenment values in Turkey.*"
+    explanationText: "<img/chp5_20251125_200815_0000.svg>"
 }];
 
 
