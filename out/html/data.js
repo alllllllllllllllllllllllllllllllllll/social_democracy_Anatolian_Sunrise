f4ff32b8d5d26c7305a5675929130412fee76359
alllllllllllllllllllllllllllllllllll <111067206+alllllllllllllllllllllllllllllllllll@users.noreply.github.com>
2025-11-28 18:56:55 +0300

    Update data.js

--- out/html/data.js
+++ out/html/data.js
@@ -3,10 +3,3 @@ const colourList = [{
         style: "color: #E3000F; font-weight: bold;"
     }
 ];
-
-
-const tooltipList = [{
-    searchString: "",
-    explanationText: "<img src=img/chp5_20251125_200815_0000.svg> Republican People's Party"
-    }
-];
