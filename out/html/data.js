ad99330d171a652e8ef76437db24bdc776771b40
alllllllllllllllllllllllllllllllllll <111067206+alllllllllllllllllllllllllllllllllll@users.noreply.github.com>
2025-11-30 15:13:49 +0300

    Update data.js

--- out/html/data.js
+++ out/html/data.js
@@ -1,11 +1,44 @@
 const tooltipList = [{
     searchString: "CHP",
-    explanationText: "<img src=img/chp5_20251125_200815_0000.svg> Republican People's Party, aka CHP."
-}];
+    explanationText: "<img src=img/chp5_20251125_200815_0000.svg> Republican People's Party, our party."
+},
+{
+    searchString: "AP",
+    explanationText: "<img src=img/AP.jpg> Justice Party, our primary adversary. Relations: [+ z_relation +]"
+}              
+];
 
 
 const colourList = [{
         word: "CHP",
         style: "color: #E3000F; font-weight: bold;"
-    }
+},
+{
+        word: "AP",
+        style: "color: #750a6f; font-weight: bold;"
+},
+{
+        word: "CGP",
+        style: "color: #5a5f63; font-weight: bold;"
+},
+{
+        word: "TIP",
+        style: "color: #8B0000; font-weight: bold;"
+},
+{
+        word: "DP",
+        style: "color: #4a4d04; font-weight: bold;"
+},
+{
+        word: "MSP",
+        style: "color: #045421; font-weight: bold;"
+},
+{
+        word: "MHP",
+        style: "color: #954B00; font-weight: bold;"
+},
+{
+        word: "SBP",
+        style: "color: #9E5747; font-weight: bold;"
+}
 ];
