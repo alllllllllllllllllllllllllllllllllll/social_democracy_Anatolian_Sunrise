2c2e758335a6c775e438872ab2e8e5dd8f2d5191
alllllllllllllllllllllllllllllllllll <111067206+alllllllllllllllllllllllllllllllllll@users.noreply.github.com>
2026-06-30 15:25:47 +0300

    Update data.js

--- out/html/data.js
+++ out/html/data.js
@@ -68,7 +68,11 @@ const colourList = [{
 {
         word: "TIP",
         style: "color: #8B0000; font-weight: bold;"
-},                  
+},
+{
+        word: "TİP",
+        style: "color: #8B0000; font-weight: bold;"
+},                    
 {
         word: "Raiders",
         style: "color: #367824; font-weight: bold;"
